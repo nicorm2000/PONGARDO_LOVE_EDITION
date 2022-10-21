d96340e03cd92858b3cad7996d8e1719309b713f
nicorm <nicorm2000@gmail.com>
2022-10-20 23:25:43 -0300

    started with ball logic and collisions

--- main.lua
+++ main.lua
@@ -3,28 +3,7 @@ function love.load()
   love.window.setTitle("PONGARDO LOVE EDITION")
   love.window.setMode(1600,800)
   
-  player1 = {}
-  player1.x = 50
-  player1.y = 300
-  player1.width = 50
-  player1.height = 250
-  player1.speed = 2
-  
-  
-  player2 = {}
-  player2.x = 1500
-  player2.y = 300
-  player2.width = 50
-  player2.height = 250
-  player2.speed = 2
-  
-  ball = {}
-  ball.x = love.graphics.getWidth() / 2
-  ball.y = love.graphics.getHeight() / 2
-  ball.radius = 20
-  ball.right = false
-  ball.up = false
-  ball.speed = 5
+  reset("P1")
   
 end
 
@@ -32,6 +11,7 @@ function love.update(dt)
   
   playerMovement(player1, "w", "s")
   playerMovement(player2, "up", "down")
+  ballMovement(ball)
   
 end
 
@@ -57,5 +37,89 @@ function playerMovement(player, upMovement, downMovement)
   if love.keyboard.isDown(downMovement) and (player.y + player.height) <= love.graphics.getHeight() then 
     player.y = player.y + player.speed
   end
+  
+end
+
+function ballMovement(ball)
+  
+  if ball.right then
+    ball.x = ball.x + ball.speed
+  else
+    ball.x = ball.x - ball.speed
+  end
+  
+  if ball.up then
+    ball.x = ball.y + ball.speed
+  else
+    ball.x = ball.y - ball.speed
+  end
+  
+  if ball.x >= player1.x 
+  and ball.x <= (player1.x + player1.width) 
+  and (ball.y - ball.radius) <= (player1.y + player1.height) 
+  and (ball.y - ball.radius) >= player1.y 
+  then
+    ball.up = false
+  end
+  
+  if ball.x >= player1.x 
+  and ball.x <= (player1.x + player1.width) 
+  and (ball.y + ball.radius) <= player1.y 
+  and (ball.y + ball.radius) >= (player1.y + player1.height) 
+  then
+    ball.up = true
+  end
+  
+  if ball.x >= player2.x 
+  and ball.x <= (player2.x + player2.width) 
+  and (ball.y - ball.radius) <= (player2.y + player2.height) 
+  and (ball.y - ball.radius) >= player2.y 
+  then
+    ball.up = false
+  end
+  
+  if ball.x >= player2.x 
+  and ball.x <= (player2.x + player2.width) 
+  and (ball.y + ball.radius) <= player2.y 
+  and (ball.y + ball.radius) >= (player2.y + player2.height) 
+  then
+    ball.up = true
+  end
+  
+  if (ball.x - ball.radius) <= (player1.x + player1.width) 
+  and (ball.x - ball.radius) >= player1.x
+  and ball.y >= player1.y
+  and ball.y <= player1.y + player1.height 
+  then
+    ball.right = true
+    ball.speed = ball.speed + 1
+  end
+  
+end
+
+function reset(playerPoints)
+  
+  player1 = {}
+  player1.x = 50
+  player1.y = 300
+  player1.width = 50
+  player1.height = 250
+  player1.speed = 2
+  
+  
+  player2 = {}
+  player2.x = 1500
+  player2.y = 300
+  player2.width = 50
+  player2.height = 250
+  player2.speed = 2
+  
+  ball = {}
+  ball.x = love.graphics.getWidth() / 2
+  ball.y = love.graphics.getHeight() / 2
+  ball.radius = 20
+  ball.right = false
+  ball.up = false
+  ball.speed = 5
   
 end
